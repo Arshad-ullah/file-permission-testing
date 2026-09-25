15429a36f99c37e77d00bb8962da2c72a8282e61
Arshad ullah <jahan665577@gmail.com>
2026-09-25 15:23:51 +0500

    issue solve

---
+++ assignment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# name, age, city
+
+read -p "Enter name " name
+
+read -p "Enter age " age
+
+read -p "Enter city " city
+
+echo "Hello $name"
+
+echo "You are $age years old and you live in ${city}."
